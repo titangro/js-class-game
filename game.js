f56af0c29f10f0af656e5d5d361e65e404b799e1
Roman <titangro@users.noreply.github.com>
2018-06-18 19:57:31 +0300

    Поправил метод createActors()

--- game.js
+++ game.js
@@ -45,7 +45,7 @@ class Actor {
 	}
 	act() {}
 	isIntersect(actor) {
-		if (actor instanceof Actor && actor !== undefined) {			
+		if (actor instanceof Actor && actor !== undefined) {
 			if (actor !== this && (actor.top === this.top && actor.bottom === this.bottom && actor.left === this.left && actor.right === this.right || actor.left > this.left && this.right > actor.right && actor.top > this.top && actor.bottom < this.bottom || (actor.left > this.right && this.left > actor.right || actor.right > this.left && this.right > actor.left) && (actor.top > this.bottom && this.top > actor.bottom || actor.bottom > this.top && this.bottom > actor.top))) {
 				return true;
 			} else {
@@ -54,7 +54,7 @@ class Actor {
 		} else {
 			throw new Error('Движущийся объект не является типом Actor');
 		}
-	}	
+	}
 }
 
 class Level {
@@ -62,33 +62,33 @@ class Level {
 		this.grid = grid;
 		this.actors = actors;
 		this.status = null;
-		this.finishDelay = 1;		
+		this.finishDelay = 1;
 	}	
 	get player() {		
-		return this.actors.find((actor) => actor.type === "player");		
+		return this.actors.find((actor) => actor.type === "player");
 	}
-	get height() {	
+	get height() {
 		return this.grid.length;
 	}
 	get width() {
-		return this.grid.reduce((result, item) => {			
+		return this.grid.reduce((result, item) => {
 			return Math.max(item.length, result);
-		}, 0);	
-	}	
+		}, 0);
+	}
 	isFinished() {
 		if (this.status !== null) {
 			return this.finishDelay < 0
 		} else {
 			return false;
-		}		
-	}	
+		}
+	}
 	actorAt(actor) {
 		if (actor instanceof Actor && actor !== undefined) {
 			if (this.actors) {
 				return this.actors.find((item) => item instanceof Actor && actor.isIntersect(item));					
 			}
-		} else {	
-			throw new Error('Объект не является типом Actor');			
+		} else {
+			throw new Error('Объект не является типом Actor');
 		}
 	}
 	obstacleAt(pos, size) {
@@ -102,30 +102,30 @@ class Level {
 				for (let posX = Math.floor(pos.x); posX < pos.x + size.x; posX++) {
 					let obstacle = this.grid[posY][posX];
 					if (obstacle) {
-						return obstacle;	
-					}												
+						return obstacle;
+					}
 				}
 			}
-		} else { 
+		} else {
 			throw new Error('Объект(ы) не является типом Vector');
 		}
 	}
-	removeActor(actor) {				
-		this.actors.splice(this.actors.indexOf(actor), 1);	
+	removeActor(actor) {
+		this.actors.splice(this.actors.indexOf(actor), 1);
 	}
 	noMoreActors(type) {
 		if (this.actors) {
 			return !(this.actors.find((actor) => actor.type === type));
 		} else {
-			return true;			
-		}		
+			return true;
+		}
 	}
 	playerTouched(type, actor) {
-		if (this.status === null) {						
+		if (this.status === null) {
 			if (type === 'lava' || type === 'fireball') {
 				this.status = 'lost';
 			} else {
-				if (type === 'coin') {			
+				if (type === 'coin') {
 					this.removeActor(actor);
 					if (this.noMoreActors('coin')) {
 						this.status = 'won';
@@ -154,24 +154,24 @@ class LevelParser {
 			return 'lava';
 		}
 	}
-	createGrid(plan) {		
-		let grid = [];		
-		if (plan.length === 0) {			
+	createGrid(plan) {
+		let grid = [];
+		if (plan.length === 0) {
 			return grid;
 		} else {
 			let parser = this;
-			plan.forEach((string) => {				
+			plan.forEach((string) => {
 				let result = string.split('');
-				grid.push(result.map((cell) => {	
-					return parser.obstacleFromSymbol(cell);										
+				grid.push(result.map((cell) => {
+					return parser.obstacleFromSymbol(cell);
 				}));
-			});					
+			});
 			return grid;
 		}
 	}
 	createActors(plan) {
 		let grid = [];
-		if (plan.length === 0 || this.data === undefined) {			
+		if (plan.length === 0 || this.data === undefined) {
 			return [];
 		} else {
 			let actor, result, item;
@@ -180,18 +180,16 @@ class LevelParser {
 				result = posX.split('');
 				result.forEach((posY, cell) => {
 					item = parser.actorFromSymbol(posY);
-					if (item !== undefined && new item() instanceof Actor) {						
+					if (item !== undefined && new item() instanceof Actor) {
 						actor = new item(new Vector(cell, column));
-					} else {
-						return grid.push();																					
-					}	
-					grid.push(actor);
-				});								
-			});		
+						grid.push(actor);
+					}
+				});
+			});
 			return grid;
 		}
 	}
-	parse(plan) {		
+	parse(plan) {
 		let level = new Level(this.createGrid(plan), this.createActors(plan));
 		return level;
 	}
@@ -199,15 +197,15 @@ class LevelParser {
 
 class Fireball extends Actor {
 	constructor(pos = new Vector(0, 0), speed = new Vector(0, 0)) {
-		super();		
+		super();
 		this.pos = pos;
 		this.speed = speed;
 		this.size = new Vector(1, 1);
-	}		
+	}
 	get type() {
 		return 'fireball';
-	}		
-	getNextPosition(time = 1) {		
+	}
+	getNextPosition(time = 1) {
  		return new Vector(this.pos.x, this.pos.y).plus(new Vector(this.speed.x, this.speed.y).times(time));
 	}
 	handleObstacle() {
@@ -215,7 +213,7 @@ class Fireball extends Actor {
 		this.speed.y = -this.speed.y;
 	}
 	act(time, grid) {
-		let newPosition = this.getNextPosition(time);					
+		let newPosition = this.getNextPosition(time);
 		if (grid.obstacleAt(newPosition, this.size)) {
 			this.handleObstacle();
 		} else {
@@ -263,20 +261,20 @@ class Coin extends Actor {
 		this.size = new Vector(0.6, 0.6);
 		if (pos) {
 			this.pos = new Vector(pos.x + 0.2, pos.y + 0.1);
-			this.start = new Vector(pos.x + 0.2, pos.y + 0.1);			
+			this.start = new Vector(pos.x + 0.2, pos.y + 0.1);
 		} else {
 			this.pos = new Vector(0.2, 0.1);
 			this.start = new Vector(0.2, 0.1);
 		}		
 		this.springSpeed = 8;
 		this.springDist = 0.07;
-		this.spring = Math.floor(Math.random() * 2 * Math.PI);		
+		this.spring = Math.floor(Math.random() * 2 * Math.PI);
 	}
 	get type() {
 		return 'coin';
 	}
-	updateSpring(time = 1) {		
-		this.spring += this.springSpeed * time; 
+	updateSpring(time = 1) {
+		this.spring += this.springSpeed * time;
 	}
 	getSpringVector() {
 		return new Vector(0, Math.sin(this.spring) * this.springDist);
